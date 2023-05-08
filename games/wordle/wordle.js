457999a7877ef72e6f9cc1164faf7119bb837b16
olavma <127435991+olavma@users.noreply.github.com>
2023-05-08 08:19:49 +0200

    El Wordle funciona
    El Wordle ahora funciona correctamente no como en la version anterior

--- games/wordle/wordle.js
+++ games/wordle/wordle.js
@@ -7,7 +7,7 @@ var comprobar = document.getElementById("check");
 
 palabraSecreta();
 
-// Funcion que elige una palabra random del documento JSON y la guarda en caracteres separados
+// Funcion que recibe un json de la API y elige una palabra random de ese JSON y la guarda en caracteres separados en la array secret
 function palabraSecreta() {
     fetch("https://apipost.azurewebsites.net/wordle")
         .then((response) => response.json())
@@ -16,6 +16,7 @@ function palabraSecreta() {
             if (json[randomIndex].palabra_Id === randomIndex + 1) {
                 const palabraElegida = json[randomIndex].palabra;
                 secret.push(...palabraElegida.split(''));
+                console.log(`Tu palabra es: ${secret}`);
             } else {
                 console.log(`No se encontró ninguna palabra con el índice ${randomIndex + 1}`);
             }
@@ -41,7 +42,7 @@ comprobar.addEventListener("click", function() {
 // Funcion que comprueba si la longitud de la palabra del usuario es igual a cinco o no
 function CheckLength(charSplit, msg) {
     if (charSplit.length != 5) {
-        msg.textContent = "Error: longitud incorrecta";
+        msg.textContent = "Error: la longitud de su palabra es inferior a 5";
         return false;
     }
     return true;
@@ -81,20 +82,24 @@ function showAnswer(charSplit, secret) {
 
 // Funcion que comprueba si cada letra coincide con la letra de la palabra secreta o no.
 function calculWordle(charSplit, divFila, seccionResult) {
-    for (i = 0; i < charSplit.length; i++) {
-        var position = 0;
-        if (secret[i] == charSplit[i]) {
-            AssingDivResult(divFila, seccionResult, "green", charSplit[i]);
-        } else if (secret[i] != charSplit[i]) {
-            for (j = 0; j < charSplit.length; j++) {
-                if (secret[j] == charSplit[j]) { position++; }
-            }
-            if (position != 0) {
-                AssingDivResult(divFila, seccionResult, "yellow", charSplit[i]);
-            } else {
-                AssingDivResult(divFila, seccionResult, "grey", charSplit[i]);
-            }
+    let nonGuessedLetters = [...secret];
+    let guessedWrongPos = [...secret];
+    for (let i = 0; i < charSplit.length; i++) {
+        if (charSplit[i] === secret[i]) {
+            let index = nonGuessedLetters.indexOf(charSplit[i]);
+            nonGuessedLetters.splice(index, 1);
         }
+    }
+    for (let i = 0; i < charSplit.length; i++) {
+        let color = 'DarkGray';
+        if (charSplit[i] === secret[i]) {
+            color = 'DarkGreen';
+        } else if (secret.includes(charSplit[i]) && nonGuessedLetters.includes(charSplit[i]) && guessedWrongPos.includes(charSplit[i])) {
+            color = 'yellow';
+            let index = guessedWrongPos.indexOf(charSplit[i]);
+            guessedWrongPos.splice(index, 1);
+        }
+        AssingDivResult(divFila, seccionResult, color, charSplit[i]);
     }
 }
 
